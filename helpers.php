fec1e597ad4cc91771ac40d1788760ec63496eaa
weilong <wilonx@163.com>
2017-11-14 14:24:57 +0800

    a parameters type bug

--- helpers.php
+++ helpers.php
@@ -46,7 +46,7 @@ if (! function_exists('timeDebug')) {
      * @param  string $separate
      * @return array
      */
-    function timeDebug(string $mark = null, $echo = true, string $separate = '')
+    function timeDebug($mark = null, $echo = true, string $separate = '')
     {
         global $timeDebug;
         $separate = $separate ?: (PHP_SAPI == 'cli' ? PHP_EOL : '<br>');
